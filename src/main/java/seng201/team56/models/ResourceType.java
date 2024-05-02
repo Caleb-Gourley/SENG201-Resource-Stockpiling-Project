5fd7cf65c2a615c37d82d82b50eddc021976ad3b
Sean Reitsma <sre114@uclive.ac.nz>
2024-05-02 15:25:07 +1200

    Documented ResourceType
    Added docstrings to the resource types and included
    links to the recipes.

--- src/main/java/seng201/team56/models/ResourceType.java
+++ src/main/java/seng201/team56/models/ResourceType.java
@@ -1,5 +1,39 @@
 package seng201.team56.models;
 
+/**
+ * ResourceType options (to be used for carts and towers)
+ * @author sean
+ */
 public enum ResourceType {
-    BCO_QUICHE, BOUILLABAISSE, GRUYER_CHEESE_SOUFFLE,CREME_BRULEE,NICOISE_SALAD,COQ_A_VIN
+    /**
+     * Bacon, Cheddar, and Onion Quiché
+     * @recipe <a href=https://www.foodandwine.com/recipes/bacon-cheddar-and-onion-quiche>bacon-cheddar-and-onion-quiche</a>
+     */
+    BCO_QUICHE,
+    /**
+     * Bouillabaisse
+     * @recipe <a href=https://www.foodandwine.com/bouillabaisse-ludo-lefebvre>bouillabaisse-ludo-lefebvre</a>
+     */
+    BOUILLABAISSE,
+    /**
+     * Gruyère Cheese Soufflé
+     * @recipe <a href=https://www.foodandwine.com/recipes/gruyere-cheese-souffle>gruyere-cheese-souffle</a>
+     */
+    GRUYER_CHEESE_SOUFFLE,
+    /**
+     * Crème Brûlée<br>
+     * Mmm...
+     * @recipe <a href=https://www.foodandwine.com/recipes/creme-brulee>creme-brulee</a>
+     */
+    CREME_BRULEE,
+    /**
+     * Hot Niçoise Salad
+     * @recipe <a href=https://www.foodandwine.com/recipes/hot-nicoise-salad>hot-nicoise-salad</a>
+     */
+    NICOISE_SALAD,
+    /**
+     * Coq au Vin
+     * @recipe <a href=https://www.foodandwine.com/recipes/coq-au-vin>coq-au-vin</a>
+     */
+    COQ_A_VIN
 }
