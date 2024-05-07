58d55d49ee2bed9c24cba972b8f3d7c6974963bb
Caleb Gourley <cgo80@uclive.ac.nz>
2024-05-07 22:20:46 +1200

    Implemented Upgrade, Round, and Purchasable
    Upgrade - Constructor, getBuyPrice, getSellPrice, getDescription, and getModiferAmount.
    Round - Constructor, getCarts, addCart, removeCart.
    Purchasable - getBuyPrice, getSellPrice, getDescription.
    
    Documentation added to both classes and interface.

--- src/main/java/seng201/team56/models/Round.java
+++ src/main/java/seng201/team56/models/Round.java
@@ -1,4 +1,42 @@
 package seng201.team56.models;
 
+import java.util.ArrayList;
+
+/**
+ * Represents a round.
+ * @author Caleb Gourley
+ */
 public class Round {
+
+    private ArrayList<Object> carts = new ArrayList<>();
+    private double trackDistance;
+    private int roundNumber;
+
+    /**
+     * Constructor
+     * Sets the initial trackDistance and roundNumber to 0
+     * @param trackDistance the trackDistance for each round
+     * @param roundNumber the roundNumber keeps track of each round
+     */
+    public Round(double trackDistance, int roundNumber) {
+        this.trackDistance = trackDistance;
+        this.roundNumber = roundNumber;
+    }
+
+    /**
+     * Getter for carts
+     * @return carts
+     */
+    public ArrayList<Object> getCarts() { return carts; }
+
+    /**
+     * Adds Cart objects to carts ArrayList for that round
+     * @param cart the cart object to be added to the ArrayList
+     */
+    public void addCart(Cart cart) { carts.add(cart); }
+
+    /**
+     * Remove Cart object from ArrayList when cart is full
+     */
+    public void removeCart() { carts.remove(-1); }
 }
