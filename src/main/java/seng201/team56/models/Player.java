cec5764b67262487117a124df6a1a5cce3399954
Sean Reitsma <sre114@uclive.ac.nz>
2024-05-08 16:42:12 +1200

    Implement Player

---
+++ src/main/java/seng201/team56/models/Player.java
@@ -0,0 +1,106 @@
+package seng201.team56.models;
+
+/**
+ * Represents the player and stores player-specific game-wide data
+ * @author Sean Reitsma
+ */
+public class Player {
+    private String name;
+    private Inventory inventory;
+    private Difficulty difficulty;
+    private double money;
+    private int xp;
+
+    /**
+     * Constructor
+     * Sets the player name, difficulty, and the initial inventory. Sets money and xp to 0.
+     * @param name
+     * @param difficulty
+     * @param startInventory
+     */
+    public Player(String name, Difficulty difficulty, Inventory startInventory) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.inventory = startInventory;
+        this.money = 0;
+        this.xp = 0;
+    }
+
+    /**
+     * Getter for name
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter for name
+     * @param name the new name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Setter for difficulty
+     * @param difficulty the new difficulty to set
+     */
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    /**
+     * Getter for the game difficulty
+     * @return difficulty
+     */
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    /**
+     * Getter for how much money the player has
+     * @return money
+     */
+    public double getMoney() {
+        return money;
+    }
+
+    /**
+     * Add money to the player
+     * @param amount the amount to add
+     */
+    public void addMoney(double amount) {
+        money += amount;
+    }
+
+    /**
+     * Subtracts amount from the player's money if the player has enough
+     * @param amount the amount to subtract
+     * @return true if the money is subtracted, false if the player does not have enough money
+     */
+    public boolean spendMoney(double amount) {
+        if (money >= amount) {
+            money -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Getter for player xp
+     * @return xp
+     */
+    public int getXp() {
+        return xp;
+    }
+
+    /**
+     * Add experience to the player
+     * @param amount xp amount to add
+     */
+    public void addXp(int amount) {
+        xp += amount;
+    }
+}
