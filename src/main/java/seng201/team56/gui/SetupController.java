3c771a7757e72136515b1846a72b0a8110d56fcc
Gourleycc18 <Gourleycc18@student.cbhs.school.nz>
2024-05-15 12:48:57 +1200

    SetupController.java and Tower_Eats_setup.fxml
    Linked up Buttons, Slider, and TextField from SceneBuilder to SetupController just need to add functionality.

--- src/main/java/seng201/team56/gui/SetupController.java
+++ src/main/java/seng201/team56/gui/SetupController.java
@@ -1,5 +1,9 @@
 package seng201.team56.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import seng201.team56.models.Difficulty;
 import seng201.team56.models.ResourceType;
@@ -10,6 +14,42 @@ import java.util.ArrayList;
 
 public class SetupController {
 
+    @FXML
+    private Button EasyButton;
+    @FXML
+    private Button MediumButton;
+    @FXML
+    private Button HardButton;
+    @FXML
+    private Button Tower1Button;
+    @FXML
+    private Button Tower2Button;
+    @FXML
+    private Button Tower3Button;
+    @FXML
+    private Button Tower4Button;
+    @FXML
+    private Button Tower5Button;
+    @FXML
+    private Button Tower6Button;
+    @FXML
+    private Button Tower7Button;
+    @FXML
+    private Button Tower8Button;
+    @FXML
+    private Button Tower9Button;
+    @FXML
+    private Button SelectedTower1Button;
+    @FXML
+    private Button SelectedTower2Button;
+    @FXML
+    private Button SelectedTower3Button;
+    @FXML
+    private Button PlayButton;
+    @FXML
+    private TextField NameTextField;
+    @FXML
+    private Slider RoundsSlider;
     private SetupService setupService;
 
     public void init(Stage stage) {
